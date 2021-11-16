22b51fab43c056f816107f2772629c06dbe5bca0
Tina Ly <t.tinaly@hotmail.com>
2021-11-15 19:01:25 -0800

    adding to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+
+const intern = require('./lib/intern')
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer')
+
+const fs = require('fs');
+
+const createTeam = require('./src/createTeam');
